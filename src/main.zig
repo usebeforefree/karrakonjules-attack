a4bd4ec6dabc5e1c9eb86ea3086af7e00c3a1aba
Petar <perazmn@gmail.com>
2026-01-31 12:38:25 +0100

    Add debug text

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const rl = @import("raylib");
 const perlin = @import("perlin.zig");
+const rg = @import("raygui");
 
 const State = struct {
     const GameState = enum {
@@ -10,9 +11,13 @@ const State = struct {
     };
 
     var phase: GameState = .intro;
+    pub fn nextPhase() void {
+        State.phase = std.meta.intToEnum(GameState, @intFromEnum(State.phase) + 1) catch State.phase;
+    }
 };
 
 var state: State = .{};
+var debug_mode: bool = true;
 
 pub fn main() anyerror!void {
     const screenWidth = 800;
@@ -22,6 +27,7 @@ pub fn main() anyerror!void {
     defer rl.closeWindow();
 
     rl.setTargetFPS(60);
+    rg.setStyle(.default, .{ .default = .text_size }, 30);
 
     const menu_texture = try rl.loadTexture("assets/menu_day.png");
 
@@ -43,6 +49,10 @@ pub fn main() anyerror!void {
         const time: f32 = @floatCast(rl.getTime());
         const offset_noise = perlin.noise(f32, perlin.permutation, .{ .x = time, .y = 34.5, .z = 345.3 }) * 100;
 
+        // INPUT
+        if (rl.isKeyPressed(.tab)) {
+            debug_mode = !debug_mode;
+        }
         rl.beginDrawing();
         defer rl.endDrawing();
 
@@ -51,5 +61,12 @@ pub fn main() anyerror!void {
         rl.drawTexturePro(menu_texture, rect, rect2, .{ .x = 0, .y = 0 }, 0, .white);
 
         rl.drawText("Karrakonjules attack!", @intFromFloat(offset_noise), 20, 100, .black);
+
+        if (debug_mode) {
+            if (rg.button(.{ .height = 35, .width = 200, .x = 10, .y = 10 }, "Next phase")) {
+                State.nextPhase();
+            }
+            _ = rg.label(.{ .height = 75, .width = 100, .x = 10, .y = 30 }, @tagName(State.phase));
+        }
     }
 }

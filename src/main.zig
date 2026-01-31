efe139091f8c279377871d2dcd7ffeb2d0763b20
nothke <nothke@gmail.com>
2026-01-31 15:23:01 +0100

    Centered sprite

--- src/main.zig
+++ src/main.zig
@@ -55,6 +55,25 @@ fn drawFullscreenCentered(tex: rl.Texture2D) void {
     tex.drawPro(getRect(tex), screen_rect, .{ .x = 0, .y = 0 }, 0, .white);
 }
 
+pub fn drawSpriteCentered(tex: rl.Texture2D, x: f32, y: f32, scale: f32, rot: f32) void {
+    const rect: rl.Rectangle = .{
+        .x = x,
+        .y = y,
+        .width = @as(f32, @floatFromInt(tex.width)) * scale,
+        .height = @as(f32, @floatFromInt(tex.height)) * scale,
+    };
+    tex.drawPro(
+        getRect(tex),
+        rect,
+        .{
+            .x = @as(f32, @floatFromInt(tex.width)) / 2,
+            .y = @as(f32, @floatFromInt(tex.height)) / 2,
+        },
+        rot,
+        .white,
+    );
+}
+
 pub fn main() anyerror!void {
     const screenWidth = 800;
     const screenHeight = 450;
@@ -68,6 +87,7 @@ pub fn main() anyerror!void {
     rg.setStyle(.default, .{ .default = .text_size }, 30);
 
     const menu_texture = try rl.loadTexture("assets/menu_day.png");
+    const sun_rays = try rl.loadTexture("assets/sun_rays.png");
 
     const rect: rl.Rectangle = .{
         .x = 0,
@@ -78,16 +98,9 @@ pub fn main() anyerror!void {
 
     _ = rect;
 
-    const screen_rect: rl.Rectangle = .{
-        .x = 0,
-        .y = 0,
-        .width = @floatFromInt(rl.getScreenWidth()),
-        .height = @floatFromInt(rl.getRenderHeight()),
-    };
-
-    _ = screen_rect;
-
     while (!rl.windowShouldClose()) {
+        const screenw: f32 = @floatFromInt(rl.getScreenWidth());
+        // const screenh: f32 = @floatFromInt(rl.getScreenHeight());
         const time: f32 = @floatCast(rl.getTime());
         const offset_noise = perlin.noise(f32, perlin.permutation, .{ .x = time, .y = 34.5, .z = 345.3 }) * 100;
 
@@ -103,6 +116,18 @@ pub fn main() anyerror!void {
         drawFullscreenCentered(menu_texture);
 
         rl.drawText("Karrakonjules attack!", @intFromFloat(offset_noise), 20, 100, .black);
+
+        const sun_rect = getRect(sun_rays);
+
+        sun_rays.drawPro(
+            getRect(sun_rays),
+            getRect(sun_rays),
+            .{ .x = sun_rect.width / 2, .y = sun_rect.height / 2 },
+            time * 20,
+            .white,
+        );
+
+        drawSpriteCentered(sun_rays, screenw / 2, 30, 1, time * 10);
 
         if (debug_mode) {
             if (rg.button(.{ .height = 35, .width = 200, .x = 10, .y = 10 }, "Next phase")) {
